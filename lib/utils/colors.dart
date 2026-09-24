e9e8471b9951ba7d95216768848e825868e02256
Nadirameidira <meisaputrinadira@gmail.com>
2026-09-25 00:00:38 +0700

    Menambahkan color palette baru, yaitu  warna baru DarkG

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color orange = Color(0xFFAB4B25);
   static const Color greenBtn = Color(0xFFC4D7AA);
   static const Color bg = Color.fromARGB(255, 238, 236, 226);
+  static const Color darkG = Color(0xFF8DB654);
 }
